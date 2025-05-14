3eea39edb1245251d36b400be9504d2c8a4dd103
Troygit config --global user.email <tydamon@hotmail.comgit config --global user.nme>
2025-05-14 21:37:00 +1000

    replaced footer image with comet icon

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,40 +1,35 @@
-//import React from 'react';
+import React from 'react';
 import f3 from '../../src/assets/f3.png';
 
 function Footer() {
   return (
-    <footer className="text-center text-white">
-        <div className="col-lg-8">
-        
-        <p className="footer-text footer-center">
+    <footer className="footer text-white">
+      <div className="footer-content">
+        <p className="footer-text">
           sifzerda
-          <a 
-            href="https://github.com/sifzerda/fiction" 
-            target="_blank" 
-            rel="noopener noreferrer" 
+          <a
+            href="https://github.com/sifzerda/fiction"
+            target="_blank"
+            rel="noopener noreferrer"
             aria-label="Visit my GitHub repository"
           >
-            <img
-              src={f3}
-              alt="fox head"
-              className="fox-head"
-              width="23"
-              height="23"
-            />
+          <span className="comet">☄️</span>  
           </a>
-          2024-25
+          2024–25
         </p>
 
-          <a href="https://react-td-portfolio.netlify.app/" target="_blank" rel="noopener noreferrer" aria-label="View my project portfolio">Portfolio</a>
-
+        <a
+          href="https://react-td-portfolio.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="View my project portfolio"
+        >
+          Portfolio
+        </a>
       </div>
     </footer>
   );
 }
 
 export default Footer;
-
-
-
-
 
